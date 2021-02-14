15cf404bd1f759e197b5a4d789c011feb2f74795
Kyle Sherrington <kylejsherrington@gmail.com>
2021-02-14 12:10:30 -0800

    Add animation and timer

--- match-app/CardCollectionViewCell.swift
+++ match-app/CardCollectionViewCell.swift
@@ -24,13 +24,23 @@ class CardCollectionViewCell: UICollectionViewCell {
         frontImageView.image = UIImage(named: card.imageName)
         
         // Reset the state of the cell by checking the flipped status of the card and then showing the front or the back imageview accordingly
+        if card.isMatched == true {
+            backImageView.alpha = 0
+            frontImageView.alpha = 0
+            return
+        }
+        else {
+            backImageView.alpha = 1
+            frontImageView.alpha = 1
+        }
+        
         if card.isFlipped == true {
             // Show the front image view
             flipUp(speed: 0)
         }
         else {
             // Show the back image view
-            flipDown(speed:0)
+            flipDown(speed:0, delay:0)
         }
     }
     
@@ -43,12 +53,30 @@ class CardCollectionViewCell: UICollectionViewCell {
         card?.isFlipped = true
     }
     
-    func flipDown(speed:TimeInterval = 0.3) {
+    func flipDown(speed:TimeInterval = 0.3, delay:TimeInterval = 0.5) {
         
-        // Flip down animation
-        UIView.transition(from: frontImageView, to: backImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delay) {
+            
+            // Flip down animation
+            UIView.transition(from: self.frontImageView, to: self.backImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+        }
+       
         
         // Set the status of the card
         card?.isFlipped = false
     }
+    
+    func remove() {
+        
+        // Make the image views invisible
+        backImageView.alpha = 0
+        
+        UIView.animate(withDuration: 0.3, delay: 0.5, options: .curveEaseOut, animations: {
+            
+            self.frontImageView.alpha = 0
+            
+        }, completion: nil)
+    }
 }
+
+
